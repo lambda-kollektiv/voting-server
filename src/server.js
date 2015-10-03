1a3ed6877d0713c6fa9180e3ce973ca150ca394f
kordano <konrad.kuehne@rocketmail.com>
2015-10-03 16:40:30 +0200

    fix typo

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import Server from "socket.io";
 export function startServer (store) {
   const io = new Server().attach(8090);
   store.subscribe(
-    () => io.emit("state", store.getStore().toJS())
+    () => io.emit("state", store.getState().toJS())
   );
   io.on("connection", (socket) => {
     socket.emit("state", store.getState().toJS());
